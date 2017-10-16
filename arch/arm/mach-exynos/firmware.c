krzk/linux-vendor-backup

ARM: EXYNOS: Change the default DMA coherent pool size
USB 3.0 external HDDs using UAS(USB Attached SCSI) have a xHCI segment
allocation problem when the xHCI ring buffer expansion is needed. This
change is to fix the xHCI ring buffer expansion problem by incresing the
DMA coherent pool size.

Change-Id: I088b9bd3eed98e11c38ca9c498908a966f300adb
Signed-off-by: Brian Kim <brian.kim@hardkernel.com>

arch/arm/mach-exynos/firmware.c
@@ -13,12 +13,14 @@
 #include <linux/init.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
+#include <linux/sizes.h>
 
 #include <asm/cacheflush.h>
 #include <asm/cputype.h>
 #include <asm/firmware.h>
 #include <asm/hardware/cache-l2x0.h>
 #include <asm/suspend.h>
+#include <asm/dma-mapping.h>
 
 #include "common.h"
 #include "smc.h"
@@ -225,6 +227,8 @@ void __init exynos_firmware_init(void)
 		outer_cache.write_sec = exynos_l2_write_sec;
 		outer_cache.configure = exynos_l2_configure;
 	}
+
+	init_dma_coherent_pool_size(SZ_1M);
 }
 
 #define REG_CPU_STATE_ADDR	(sysram_ns_base_addr + 0x28)

mihailescu2m/linux

Signed-off-by: memeka <mihailescu2m@gmail.com>
Signed-off-by: Dongjin Kim <tobetter@gmail.com>

arch/arm/mach-exynos/firmware.c
@@ -13,6 +13,8 @@
 #include <linux/init.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
+#include <linux/dma-mapping.h>
+#include <linux/sizes.h>
 
 #include <asm/cacheflush.h>
 #include <asm/cputype.h>
@@ -225,6 +227,8 @@ void __init exynos_firmware_init(void)
 		outer_cache.write_sec = exynos_l2_write_sec;
 		outer_cache.configure = exynos_l2_configure;
 	}
+
+	init_dma_coherent_pool_size(SZ_1M);
 }
 
 #define REG_CPU_STATE_ADDR	(sysram_ns_base_addr + 0x28)